d4b8ac65fbe2dd72fa6af28fd3e864fbef972d18
Nicolas Barzotti <nicolasbarzotti@gmail.com>
2024-04-22 23:03:46 +0200

    Finished implementing full phugoid model, added nicer variable names

--- phugoid/phugoid.hs
+++ phugoid/phugoid.hs
@@ -1,9 +1,5 @@
+import Data.List
 type Dati a = (a,a,a,a)
---type Dati = (Floating a,Floating b,Floating c,Floating d)
-
-lunghezza :: [a] -> Int
-lunghezza []        = 0
-lunghezza (x:xs)    = 1 + lunghezza xs
 
 estrai :: [Dati a] -> Int -> Dati a
 estrai (x:xs) 0 = x
@@ -13,31 +9,31 @@ linspace :: Int -> Double -> Double -> Double -> [Double]
 linspace i low up pts | i >= 0    = low : linspace (i - 1) (low + (up / pts)) up pts
                       | otherwise = []
 
-respecSum :: (Num a) => Dati a -> Dati a -> Dati a
-respecSum (a1,b1,c1,d1) (a2,b2,c2,d2) = (a1+a2, b1+b2, c1+c2, d1+d2)
+sommaTupla :: (Num a) => Dati a -> Dati a -> Dati a
+sommaTupla (a1,b1,c1,d1) (a2,b2,c2,d2) = (a1+a2, b1+b2, c1+c2, d1+d2)
 
-respecMul :: (Num a) => Dati a -> Dati a -> Dati a
-respecMul (a1,b1,c1,d1) (a2,b2,c2,d2) = (a1*a2, b1*b2, c1*c2, d1*d2)
+moltiplicaTupla :: (Num a) => Dati a -> Dati a -> Dati a
+moltiplicaTupla (a1,b1,c1,d1) (a2,b2,c2,d2) = (a1*a2, b1*b2, c1*c2, d1*d2)
 
-scalarMul :: (Num a) => Dati a -> a -> Dati a
-scalarMul (a1,b1,c1,d1) b = (a1*b, b1*b, c1*b, d1*b)
+moltiplicaTuplaPerScalare :: (Num a) => Dati a -> a -> Dati a
+moltiplicaTuplaPerScalare (a1,b1,c1,d1) b = (a1*b, b1*b, c1*b, d1*b)
 
-rhs :: (Num a, Floating a) => Dati a -> Dati a
-rhs (v,theta,x,y) =(- (9.81 * sin theta) - (1 / 40)*9.81/30.0**2*v**2,
-                    - (9.81 * cos theta / v) + 9.81/30.0**2*v,
-                       v*cos theta,
-                       v*sin theta)
+rhs :: (Num a, Floating a) => Dati a -> Dati a-> Dati a
+rhs (v,theta,x,y) (cG@costGrav, vT@vTrim, cR@coeffResist, cP@coeffPort) =(- (cG * sin theta) - (cR / cP)*cG/vT**2*v**2,
+                                                                         - (cG * cos theta / v) + cG/vT**2*v,
+                                                                         v*cos theta,
+                                                                         v*sin theta)
 
-eulerStep :: (Num a, Floating a) => Dati a -> a -> Dati a
-eulerStep datum dt = respecSum datum (scalarMul (rhs datum) dt)
+eulerStep :: (Num a, Floating a) => Dati a -> Dati a -> a -> Dati a
+eulerStep dA@datiAliante dS@datiSimulazione dt = sommaTupla dA (moltiplicaTuplaPerScalare (rhs dA dS) dt)
 
-forloop :: (Num a, Floating a) => Dati a -> a -> Int -> Int -> [Dati a]
-forloop datum dt i len  | i == 0 = datum : forloop datum dt (i+1) len 
-                        | i == (len - 1) = [datum2]
-                        | otherwise = datum2 : forloop datum2 dt (i+1) len
-                        where
-                            datum2 = eulerStep datum dt 
+calcEuler :: (Num a, Floating a) => Dati a -> Dati a -> a -> Int -> Int -> [Dati a]
+calcEuler dA@datiAliante dS@datiSimulazione dt i len| i == 0            = dA : calcolaProssimoPunto
+                                                    | i == (len - 1)    = [dA2]
+                                                    | otherwise         = dA2 : calcolaProssimoPunto
+                                                    where
+                                                        dA2 = eulerStep dA dS dt 
+                                                        calcolaProssimoPunto = calcEuler dA2 dS dt (i+1) len
 
 mainEuler :: (Num a, Fractional a, Floating a) => [Dati a]
-mainEuler = forloop (30.0, 0.0, 0.0, 1000.0) 0.1 0 1000 
---Change for loop, change all the constraints
+mainEuler = calcEuler (30.0, 0.0, 0.0, 1000.0) (9.81, 30.0, 0.05, 1.0) 0.1 0 1000 
