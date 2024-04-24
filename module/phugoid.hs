ee040f97a01496046857f032a0da9880ebbb56cd
Gabriele Ramagnano <gabrieleramagnano126@gmail.com>
2024-04-24 12:24:54 +0200

    errors solved on phugoid 's module

--- module/phugoid.hs
+++ module/phugoid.hs
@@ -5,22 +5,35 @@ import Data.List
 main :: IO ()
 main = do putStrLn "Inserisci la larghezza del passo temporale: "
           dt <- getLine 
-          let zt    = 100   {- Dato simulazione: profondità al di sotto di una linea orizzonatale di riferimento -}
-          let g     = 9.81  {- Dato simulazione accelerazione di gravità -}
-          let z0    = 100.0 {- Dato aliante: altitudine -}
-          let b0    = 10.0  {- Dato aliante: velocità ascendente risultante dalla raffica -}
-          let t     = 100.0 {- Tempo di volo finale -}
-          let nt    = calcNumPassi t dt
-          putStrLn $ show (calcEulero (z0, b0) (zt, g) dt 0 nt )
+         {- let zt = profondita     {- Dato simulazione: profondità al di sotto di una linea orizzonatale di riferimento -}
+          let g  = acceleraGravit {- Dato simulazione accelerazione di gravità -}
+          let z0 = altitudine     {- Dato aliante: altitudine -}
+          let b0 = velocita       {- Dato aliante: velocità ascendente risultante dalla raffica -}
+          let t  = tempoVolo      {- Tempo di volo finale -}
+          let nt = numPassiTemp   -- t / (read dt :: Float) numero di passi temporali -}
+          putStrLn $ show (calcEulero (z0, b0) (zt, g) ((read dt :: Float) ) 0 nt )
 
 -- mainEuler :: (Num a, Fractional a, Floating a) => [Dati a]
 -- mainEuler = calcEulero (1000.0, 10.0) (100, 9.81) 0.1 0 1000 
+--zt :: Floating
+zt = 100.0 
+--g  :: Floating
+g  = 9.81
+--z0 :: Floating
+z0 = 100.0
+--b0 :: Floating
+b0 = 10.0
+--t  :: Floating
+t  = 100.0
+--nt :: Int
+nt = 1000
+
 
 type Dati a = (a,a)
 
-{- numero di passi temporali -}
+{- numero di passi temporali 
 calcNumPassi :: Float -> Float -> Int
-calcNumPassi = t dt = t / dt
+calcNumPassi t dt = t / dt -}
 
 estrai :: [Dati a] -> Int -> Dati a
 estrai (x:xs) 0 = x
