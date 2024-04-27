040d4355e002cdbb758bd4977e90b04847aace12
Gabriele Ramagnano <gabrieleramagnano126@gmail.com>
2024-04-27 13:12:07 +0200

    first version of convection with modules

--- control/code/convection/modulo/main_convezione.hs
+++ control/code/convection/modulo/main_convezione.hs
@@ -9,11 +9,11 @@ import CondIniziali
 main_convezione :: Int -> Double -> [Double]
 main_convezione nx dt = calcConvTempo (condizioneIniziale nx lmtInf lmtSup) nt nx c dx dt
                             where
-                              lmtInf = 0.0                                   -- limite inferiore del dominio spaziale 
-                              lmtSup = 2.0                                   -- limite superiore del dominio spaziale
-                              nt = 25                                        -- numero complessivo di passi temporali che deve effettuare l'algoritmo 
-                              dx = limiteSup / (fromIntegral(nx :: Int) - 1) -- distanza tra qualsiasi coppia di punti della griglia adiacenti 
-                              c  = 1.0                                       -- velocità dell'onda 
+                              lmtInf = 0.0                                -- limite inferiore del dominio spaziale 
+                              lmtSup = 2.0                                -- limite superiore del dominio spaziale
+                              nt = 25                                     -- numero complessivo di passi temporali che deve effettuare l'algoritmo 
+                              dx = lmtSup / (fromIntegral(nx :: Int) - 1) -- distanza tra qualsiasi coppia di punti della griglia adiacenti 
+                              c  = 1.0                                    -- velocità dell'onda 
 
 {- Funzione per il calcolo numerico dell'integrazione dell'equazione di convezione lineare unidimensionale rispetto al tempo:
     *Caso base: se il numero di passi temporali è pari a zero, allora si restituisce la funzione d'onda quadra.
