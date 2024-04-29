1d4fb4ac6b0ff4d2bf541f21418337daa7389438
Nicolas Barzotti <nicolasbarzotti@gmail.com>
2024-04-29 11:22:48 +0200

    Spostate le funzioni per avere il main in cima ed esportarlo tramite modulo. aggiunto un parametro al mainFugoide che è il passo temporale

--- control/code/phugoid/phugoid.hs
+++ control/code/phugoid/phugoid.hs
@@ -1,22 +1,17 @@
-import Data.List
+module Fugoide(mainFugoide) where
+
 type Dati a = (a,a,a,a)
 
-estrai :: [Dati a] -> Int -> Dati a
-estrai (x:xs) 0 = x
-estrai (_:xs) i = estrai xs (i-1)
+mainFugoide :: (Num a, Fractional a, Floating a) => a -> [a]
+mainFugoide dt = calcEulero (30.0, 0.0, 0.0, 1000.0) (9.81, 30.0, 0.05, 1.0) dt 0 1000 
 
-linspace :: Int -> Double -> Double -> Double -> [Double]
-linspace i low up pts | i >= 0    = low : linspace (i - 1) (low + (up / pts)) up pts
-                      | otherwise = []
-
-sommaTupla :: (Num a) => Dati a -> Dati a -> Dati a
-sommaTupla (a1,b1,c1,d1) (a2,b2,c2,d2) = (a1+a2, b1+b2, c1+c2, d1+d2)
-
-moltiplicaTupla :: (Num a) => Dati a -> Dati a -> Dati a
-moltiplicaTupla (a1,b1,c1,d1) (a2,b2,c2,d2) = (a1*a2, b1*b2, c1*c2, d1*d2)
-
-moltiplicaTuplaPerScalare :: (Num a) => Dati a -> a -> Dati a
-moltiplicaTuplaPerScalare (a1,b1,c1,d1) b = (a1*b, b1*b, c1*b, d1*b)
+calcEulero :: (Num a, Floating a) => Dati a -> Dati a -> a -> Int -> Int -> [a]
+calcEulero dA@(dAA,_,_,_) dS@datiSimulazione dt i len| i == 0            = dAA : calcolaProssimoPunto
+                                                     | i == (len - 1)    = [dBA]
+                                                     | otherwise         = dBA : calcolaProssimoPunto
+                                                     where
+                                                        dB@(dBA,_,_,_) = metodoEulero dA dS dt 
+                                                        calcolaProssimoPunto = calcEulero dB dS dt (i+1) len
 
 rhs :: (Num a, Floating a) => Dati a -> Dati a-> Dati a
 rhs (v,theta,x,y) (cG@costGrav, vT@vTrim, cR@coeffResist, cP@coeffPort) =(- (cG * sin theta) - (cR / cP)*cG/vT**2*v**2,
@@ -27,13 +22,11 @@ rhs (v,theta,x,y) (cG@costGrav, vT@vTrim, cR@coeffResist, cP@coeffPort) =(- (cG 
 metodoEulero :: (Num a, Floating a) => Dati a -> Dati a -> a -> Dati a
 metodoEulero dA@datiAliante dS@datiSimulazione dt = sommaTupla dA (moltiplicaTuplaPerScalare (rhs dA dS) dt)
 
-calcEulero :: (Num a, Floating a) => Dati a -> Dati a -> a -> Int -> Int -> [a]
-calcEulero dA@(dAA,_,_,_) dS@datiSimulazione dt i len| i == 0            = dAA : calcolaProssimoPunto
-                                                     | i == (len - 1)    = [dBA]
-                                                     | otherwise         = dBA : calcolaProssimoPunto
-                                                     where
-                                                        dB@(dBA,_,_,_) = metodoEulero dA dS dt 
-                                                        calcolaProssimoPunto = calcEulero dB dS dt (i+1) len
+sommaTupla :: (Num a) => Dati a -> Dati a -> Dati a
+sommaTupla (a1,b1,c1,d1) (a2,b2,c2,d2) = (a1+a2, b1+b2, c1+c2, d1+d2)
 
-mainEuler :: (Num a, Fractional a, Floating a) => [a]
-mainEuler = calcEulero (30.0, 0.0, 0.0, 1000.0) (9.81, 30.0, 0.05, 1.0) 0.1 0 1000 
+moltiplicaTupla :: (Num a) => Dati a -> Dati a -> Dati a
+moltiplicaTupla (a1,b1,c1,d1) (a2,b2,c2,d2) = (a1*a2, b1*b2, c1*c2, d1*d2)
+
+moltiplicaTuplaPerScalare :: (Num a) => Dati a -> a -> Dati a
+moltiplicaTuplaPerScalare (a1,b1,c1,d1) b = (a1*b, b1*b, c1*b, d1*b)
--- control/code/phugoid/simplephugoid.hs
+++ control/code/phugoid/simplephugoid.hs
@@ -1,20 +1,8 @@
-import Data.List
+module FugoideSemplice(mainFugoide) where
 type Dati a = (a,a)
 
-sommaTupla :: (Num a) => Dati a -> Dati a -> Dati a
-sommaTupla (a1,b1) (a2,b2) = (a1+a2, b1+b2)
-
-moltiplicaTupla :: (Num a) => Dati a -> Dati a -> Dati a
-moltiplicaTupla (a1,b1) (a2,b2) = (a1*a2, b1*b2)
-
-moltiplicaTuplaPerScalare :: (Num a) => Dati a -> a -> Dati a
-moltiplicaTuplaPerScalare (a1,b1) b = (a1*b, b1*b)
-
-rhs :: (Num a, Floating a) => Dati a -> Dati a-> Dati a
-rhs (y@altitude, v@velocity) (cG@costGrav, zt) = (v,cG * (1-y/zt))
-
-metodoEulero :: (Num a, Floating a) => Dati a -> Dati a -> a -> Dati a
-metodoEulero dA@datiAliante dS@datiSimulazione dt = sommaTupla dA (moltiplicaTuplaPerScalare (rhs dA dS) dt)
+mainFugoide :: (Num a, Fractional a, Floating a) => a -> [a]
+mainFugoide dt = 100.0 : calcEulero (100.0, 10.0) (9.81, 100) dt 1 10000
 
 calcEulero :: (Num a, Floating a) => Dati a -> Dati a -> a -> Int -> Int -> [a]
 calcEulero dA@(dAA,_) dS@datiSimulazione dt i len   | i == (len - 1)    = [dBA]
@@ -24,5 +12,18 @@ calcEulero dA@(dAA,_) dS@datiSimulazione dt i len   | i == (len - 1)    = [dBA]
                                                         calcolaProssimoPunto = calcEulero dB dS dt (i+1) len
 
 
-mainEuler :: (Num a, Fractional a, Floating a) => [a]
-mainEuler = 100.0 : calcEulero (100.0, 10.0) (9.81, 100) 0.01 1 10000
+
+metodoEulero :: (Num a, Floating a) => Dati a -> Dati a -> a -> Dati a
+metodoEulero dA@datiAliante dS@datiSimulazione dt = sommaTupla dA (moltiplicaTuplaPerScalare (rhs dA dS) dt)
+
+rhs :: (Num a, Floating a) => Dati a -> Dati a-> Dati a
+rhs (y@altitude, v@velocity) (cG@costGrav, zt) = (v,cG * (1-y/zt))
+
+sommaTupla :: (Num a) => Dati a -> Dati a -> Dati a
+sommaTupla (a1,b1) (a2,b2) = (a1+a2, b1+b2)
+
+moltiplicaTupla :: (Num a) => Dati a -> Dati a -> Dati a
+moltiplicaTupla (a1,b1) (a2,b2) = (a1*a2, b1*b2)
+
+moltiplicaTuplaPerScalare :: (Num a) => Dati a -> a -> Dati a
+moltiplicaTuplaPerScalare (a1,b1) b = (a1*b, b1*b)
