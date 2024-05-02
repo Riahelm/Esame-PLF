3260d17d86daf39df503cc0cd53df658dd5bd7cd
Nicolas Barzotti <nicolasbarzotti@gmail.com>
2024-05-02 18:08:57 +0200

    Spostati i file condivisi nella apposita cartella

---
+++ control/src/fugoide/condivisi/condizioni_iniziali.hs
@@ -0,0 +1,15 @@
+module CondizioniIniziali where
+
+import DatiProblema 
+
+--v0 :: Float       {- Velocita' iniziale del velivolo, in m/s -}
+--v0 = vT
+
+--theta0 :: Float   {- Angolo di movimento del velivolo, in radianti -}
+--theta0 = 0.0
+
+--x0 :: Float       {- Numero di metri percorsi dal velivolo -}
+--x0 = 0.0
+
+--y0 :: Float       {- Altitudine del velivolo -}
+--y0 = 1000.0
