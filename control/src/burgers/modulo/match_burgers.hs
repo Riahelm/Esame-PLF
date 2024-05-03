cdc03e41da68dbd316d693e3960832a2d185f35e
Gabriele Ramagnano <gabrieleramagnano126@gmail.com>
2024-05-03 11:16:35 +0200

    MB: from globa to local (param)

--- control/src/burgers/modulo/match_burgers.hs
+++ control/src/burgers/modulo/match_burgers.hs
@@ -4,15 +4,12 @@ import Funzionalita
 import ParametriBurg
 import CondInizialiBurg
 
-type Dati a = (a,a,a)
+type Dati a = (a,a)
 
 
 main_burgers :: Int -> [Double]
-main_burgers nx = calcConvTempo onda nt (condBordoInf) 
-                                        (condBordoSup) 
-                                        (passoEulero)
+main_burgers nx = calcConvTempo (condizioneIniziale nx lmtInf lmtSup) (dx, dt) nt condBordoInf condBordoSup passoEulero
                      where
-                       onda = condizioneIniziale nx lmtInf lmtSup
                        lmtInf = 0.0                                -- limite inferiore del dominio spaziale 
                        lmtSup = 2.0 * pi                           -- limite superiore del dominio spaziale 
                        dx = lmtSup / (fromIntegral(nx :: Int) - 1) -- distanza tra qualsiasi coppia di punti della griglia adiacenti 
@@ -22,27 +19,25 @@ main_burgers nx = calcConvTempo onda nt (condBordoInf)
                        nt = truncate(tFine / dt)                   -- numero complessivo di passi temporali che deve effettuare l'algoritmo 
 
 
-calcConvTempo :: [Double] -> Int -> ([Double] -> Double) -> 
-                                    ([Double] -> Double) -> 
-                                    ([Double] -> Int -> Double) -> [Double]
+calcConvTempo :: [Double] -> Int -> Dati Double -> ([Double] -> Dati Double -> Double) -> 
+                                                   ([Double] -> Dati Double -> Double) -> 
+                                                   ([Double] -> Dati Double -> Int -> Double) -> [Double]
+calcConvTempo onda 0 _ _ _ _          = onda
+calcConvTempo onda nt dati inf sup pe = calcConvTempo ((inf onda dati): calcConvSpazio onda 1 sup pe) (nt - 1) inf sup pe 
 
-calcConvTempo onda 0 _ _ _       = onda
-calcConvTempo onda nt inf sup pe = calcConvTempo ((inf onda): calcConvSpazio onda 1 sup pe) (nt - 1) inf sup pe 
+calcConvSpazio :: [Double] -> Dati Double -> Int -> ([Double] -> Dati Double -> Double) -> 
+                                                    ([Double] -> Dati Double -> Int -> Double) -> [Double]
+calcConvSpazio lx dati i sup pe | i == length lx - 1 = [sup lx dati]
+                                | otherwise          = (pe lx dati i) : calcConvSpazio lx (i+1) sup pe 
 
-calcConvSpazio :: [Double] -> Int -> ([Double] -> Double) -> 
-                                     ([Double] -> Int -> Double) -> [Double]
-
-calcConvSpazio lx i sup pe | i == length lx - 1 = [sup lx]
-                           | otherwise          = (pe lx i) : calcConvSpazio lx (i+1) sup pe 
-
-condBordoInf :: [Double] -> Double
-condBordoInf onda = head onda - head onda * dt/dx * (head onda - last onda) + 
-                    nu*dt/dx**2 * ((head $ tail onda) - 2*(head onda) + last onda)
+condBordoInf :: [Double] -> Dati Double -> Double
+condBordoInf onda (dx,dt) = head onda - head onda * dt/dx * (head onda - last onda) + 
+                            nu*dt/dx**2 * ((head $ tail onda) - 2*(head onda) + last onda)
             
-condBordoSup :: [Double] -> Double
-condBordoSup lx = (last lx) - (last lx) * dt/dx * ((last lx) - (last $ init lx)) + 
-                  nu*dt/dx**2*(head lx - 2*(last lx) + (last $ init lx))
+condBordoSup :: [Double] -> Dati Double ->  Double
+condBordoSup lx (dx,dt) = (last lx) - (last lx) * dt/dx * ((last lx) - (last $ init lx)) + 
+                          nu*dt/dx**2*(head lx - 2*(last lx) + (last $ init lx))
 
-passoEulero :: [Double] -> Int -> Double
-passoEulero lx i  = (lx !! i - lx !! i * dt/dx * (lx !! i - lx !! (i-1)) + 
-                    nu*dt/dx**2*(lx !! (i+1) - 2*(lx !! i) + lx !! (i-1))) 
+passoEulero :: [Double] -> Dati Double ->  Int ->  Double
+passoEulero lx (dx,dt) i  = (lx !! i - lx !! i * dt/dx * (lx !! i - lx !! (i-1)) + 
+                            nu*dt/dx**2*(lx !! (i+1) - 2*(lx !! i) + lx !! (i-1))) 
