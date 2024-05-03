0a0f9d0835062470a468da9c751b5b97a2988cc0
Gabriele Ramagnano <gabrieleramagnano126@gmail.com>
2024-05-03 14:05:53 +0200

    init cond for burg and conv fixed:ok!

--- control/src/convection/modulo/condizioni_iniziali_uni.hs
+++ control/src/convection/modulo/condizioni_iniziali_uni.hs
@@ -2,8 +2,9 @@ module CondizioniIniziali where
 
 import CondInizialiConv 
 import CondInizialiBurg
+import Funzionalita
 
-condizioneIniziale :: Int -> (Double -> Double) -> Double -> [Double]
+condizioneIniziale :: Int -> (Double -> Double) -> Double -> Double -> [Double]
 condizioneIniziale nx onda lmtInf lmtSup = [onda x | x <- lx]
         where
           lx = calcPuntiEqui 0 nx lmtInf lmtSup
