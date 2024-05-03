06c674519bfd6ebd6965d488dfce6f474a1bb0b6
Nicolas Barzotti <nicolasbarzotti@gmail.com>
2024-05-03 16:42:41 +0200

    Aggiunto commenti al moto fugoide semplice

--- control/src/fugoide/semplice/modulo/condizioniSemplici.hs
+++ control/src/fugoide/semplice/modulo/condizioniSemplici.hs
@@ -3,7 +3,7 @@ module CondizioniSemplici where
 import DatiSemplici
 
 --z0 :: Float   
---z0 = zt    {- Altitudine iniziale del velivolo -}
+--z0 = zt       {- Altitudine iniziale del velivolo -}
 
 --b0 :: Float   
 --b0 = 10.0     {- Angolo iniziale del velivolo, in radianti -}
