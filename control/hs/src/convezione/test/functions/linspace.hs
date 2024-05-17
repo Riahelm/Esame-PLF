97bf229a8c72e62351419d3a08bd53e88dbc43b2
Gabriele Ramagnano <gabrieleramagnano126@gmail.com>
2024-05-17 12:23:44 +0200

    create linspace.hs, alternative better than calcPuntiEqui: to insert in burgers and convezione

---
+++ control/hs/src/convezione/test/functions/linspace.hs
@@ -0,0 +1,8 @@
+
+calcPuntiEqui :: Int -> Int -> Double -> Double -> [Double]
+calcPuntiEqui i nx inf sup | i == nx    = []
+                           | otherwise  = calcPunti i (nx - 1) inf (sup  - inf) 
+
+calcPunti :: Int -> Int -> Double -> Double -> [Double]
+calcPunti i nx inf dst | i == nx   = [inf]
+                       | otherwise = inf : calcPunti (i + 1) nx (inf + (dst / fromIntegral(nx :: Int))) dst
