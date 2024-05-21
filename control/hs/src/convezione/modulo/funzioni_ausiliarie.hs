3144b0659da67e7511f023a18f582785a9fbcc3a
Gabriele Ramagnano <gabrieleramagnano126@gmail.com>
2024-05-21 15:07:42 +0200

    linspace in funzioni_ausiliarie.hs modified

--- control/hs/src/convezione/modulo/funzioni_ausiliarie.hs
+++ control/hs/src/convezione/modulo/funzioni_ausiliarie.hs
@@ -3,6 +3,11 @@ module Funzionalita where
 import Data.List
 
 {- Funzione che genera un numero finito di punti tutti equidistanti tra loro in un intervallo specificato -}
+
 calcPuntiEqui :: Int -> Int -> Double -> Double -> [Double]
-calcPuntiEqui i nx inf sup  | i > nx     = []
-                            | otherwise  = inf : calcPuntiEqui (i + 1) nx (inf + (sup / fromIntegral(nx :: Int))) sup 
+calcPuntiEqui i nx inf sup | i == nx    = []
+                           | otherwise  = calcPunti i (nx - 1) inf (sup  - inf) 
+
+calcPunti :: Int -> Int -> Double -> Double -> [Double]
+calcPunti i nx inf dst | i == nx   = [inf]
+                       | otherwise = inf : calcPunti (i + 1) nx (inf + (dst / fromIntegral(nx :: Int))) dst
