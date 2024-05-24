16fcbe6ecc8f7295c54a7406066bc182c665597c
Nicolas Barzotti <nicolasbarzotti@gmail.com>
2024-05-24 17:58:27 +0200

    Aggiunta cartella di consegna, provvista di compilatori

---
+++ consegna/hs/compile-run-delete.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+ghc src/simulazioni_numeriche.hs -o bin simulazioni_numeriche
+
+rm -r src/*.{o, hi}
+
+./bin/simulazioni_numeriche
+
+rm -r bin/simulazioni_numeriche
---
+++ consegna/pro/compile-run-delete.sh
@@ -0,0 +1,7 @@
+#!bin/bash
+
+gplc src/simulazioni_numeriche.pro -o bin simulazioni_numeriche
+
+./simulazioni_numeriche
+
+rm -r simulazioni_numeriche
