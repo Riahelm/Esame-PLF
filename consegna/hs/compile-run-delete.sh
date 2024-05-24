6052318a0fe25f8061f940cf4274a7edf433c740
Nicolas Barzotti <nicolasbarzotti@gmail.com>
2024-05-24 17:59:04 +0200

    Rinominati file shell

--- consegna/hs/compile-run-delete.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-ghc src/simulazioni_numeriche.hs -o bin simulazioni_numeriche
-
-rm -r src/*.{o, hi}
-
-./bin/simulazioni_numeriche
-
-rm -r bin/simulazioni_numeriche
--- consegna/pro/compile-run-delete.sh
+++
@@ -1,7 +0,0 @@
-#!bin/bash
-
-gplc src/simulazioni_numeriche.pro -o bin simulazioni_numeriche
-
-./simulazioni_numeriche
-
-rm -r simulazioni_numeriche
