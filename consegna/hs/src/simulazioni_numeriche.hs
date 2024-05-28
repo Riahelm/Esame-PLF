0cd16f97ffa8da6b752d6cfbe8048074ed9505ae
Nicolas Barzotti <nicolasbarzotti@gmail.com>
2024-05-28 21:52:53 +0200

    Commenti

--- consegna/hs/src/simulazioni_numeriche.hs
+++ consegna/hs/src/simulazioni_numeriche.hs
@@ -1,5 +1,5 @@
-module SimulazioniNumeriche where
 
+{- Programma Haskell per effettuare simulazioni numeriche -}
 import Data.List {- necessario per usare: 
                     - !!,      che estrae l'n-esimo elemento di una lista;
                     - head,    che estrae il primo elemento di una lista;
